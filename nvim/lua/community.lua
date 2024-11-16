66ebca8fb8a614ed2238a0f0370928392eaa1870
darkiiiiiice <darkiiiiiice@gmail.com>
2024-11-16 23:24:23 +0800

    add ollama to nvim

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -11,7 +11,7 @@ return {
   -- import/override with your plugins folder
   { "ellisonleao/gruvbox.nvim", priority = 1000 },
   -- {"github/copilot.vim"},
-  -- { "Exafunction/codeium.vim", event = "BufEnter" },
+  { "Exafunction/codeium.vim", event = "BufEnter" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
@@ -27,7 +27,7 @@ return {
     keys = {
       -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
       {
-        "<leader>oo",
+        "<leader>mo",
         ":<c-u>lua require('ollama').prompt()<cr>",
         desc = "ollama prompt",
         mode = { "n", "v" },
@@ -35,7 +35,7 @@ return {
 
       -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
       {
-        "<leader>oG",
+        "<leader>mG",
         ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
         desc = "ollama Generate Code",
         mode = { "n", "v" },
@@ -50,6 +50,24 @@ return {
         args = { "serve" },
         stop_command = "pkill",
         stop_args = { "-SIGTERM", "ollama" },
+      },
+      prompts = {
+        Ask_About_Code = {
+          prompt = "我有一个问题关于: $input\n\n 代码内容如下: \n```$ftype\n$sel```,请用中文回复",
+          input_label = "Question >>> ",
+        },
+        Explain_Code = {
+          prompt = "请用中文解释这段代码,代码内容如下: \n```$ftype\n$sel```",
+        },
+        Raw = {
+          prompt = "请用中文回答问题: $input",
+          input_label = ">",
+          action = "display",
+        },
+        Sample_Prompt = {
+          prompt = "请解释这段$ftype代码的意思, 代码内容如下 $sel",
+          input_label = "> ",
+        },
       },
     },
   },
